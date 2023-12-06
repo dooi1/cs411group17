598c316431ccd9e8274536b524e8f5422c825ddb
keith <keithy0157@gmail.com>
2023-12-06 00:16:28 -0500

    welcomepage done

--- Server/client/src/WelcomePage.js
+++ Server/client/src/WelcomePage.js
@@ -5,6 +5,9 @@ import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import { brown } from '@mui/material/colors';
 
+// Background image
+const customImage = '/homepage.png';
+
 const ColorButton = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(brown[500]),
   backgroundColor: brown[500],
@@ -20,6 +23,22 @@ const styles = {
     alignItems: 'center',
     justifyContent: 'center',
     height: '100vh',
+    backgroundImage: `url(${customImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    position: 'relative',
+  },
+  overlay: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)', // Adjust opacity here
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   welcomePage: {
     textAlign: 'center',
@@ -29,12 +48,14 @@ const styles = {
 const WelcomePage = () => {
   return (
     <div style={styles.body}>
-      <div style={styles.welcomePage}>
-        <h1>Meal + Music</h1>
-        <p>Discover delicious recipes paired with great music!</p>
-        <Link to="/ingredientspage">
-          <ColorButton variant="contained">Get Started</ColorButton>
-        </Link>
+      <div style={styles.overlay}>
+        <div style={styles.welcomePage}>
+          <h1>Meal + Music</h1>
+          <p>Discover delicious recipes paired with great music!</p>
+          <Link to="/ingredientspage">
+            <ColorButton variant="contained">Get Started</ColorButton>
+          </Link>
+        </div>
       </div>
     </div>
   );
